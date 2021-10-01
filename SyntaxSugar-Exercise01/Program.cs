68043f925db26782ba261aa2035ad0e623593e31
lostinthestack <lostinthestack@protonmail.com>
2021-10-01 12:48:43 -0500

    init

---
+++ SyntaxSugar-Exercise01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SyntaxSugar_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ORIGINAL:
+            //int answer = 4;
+            //string response;
+
+            //if (answer<9)
+            //{
+            //    response = answer + "is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+
+            var answer = 4;
+
+            var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+        }
+    }
+}
